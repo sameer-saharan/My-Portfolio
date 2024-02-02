7224325dcc681b49c3371da7f2665e59fc5ad691
Sameer Saharan <albaadigoru@gmail.com>
2024-02-03 00:30:41 +0530

    javascript

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const image = document.querySelector("#jspr");
+
+const jsprojects = "https://javascript-projects-sameer.vercel.app/"
+
+image.addEventListener('click', () => {
+    window.open(jsprojects, "_blank")
+})
