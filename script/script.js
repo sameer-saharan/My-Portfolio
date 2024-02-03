f39c40410704d9dc5ff8e63a930ae0e8f89aceec
Sameer Saharan <albaadigoru@gmail.com>
2024-02-03 15:39:52 +0530

    updated script

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// JS Projects
 const image = document.querySelector("#jspr");
 
 const jsprojects = "https://javascript-projects-sameer.vercel.app/"
@@ -5,3 +6,55 @@ const jsprojects = "https://javascript-projects-sameer.vercel.app/"
 image.addEventListener('click', () => {
     window.open(jsprojects, "_blank")
 })
+
+// LinkedIn Profile
+const linkedinImage = document.querySelector("#linkedin-img")
+const linkedinUrl = "https://www.linkedin.com/in/sameer-saharan"
+
+linkedinImage.addEventListener('click', () => {
+    window.open(linkedinUrl, "_blank")
+})
+
+// Github Profile
+const githubImage = document.querySelector("#github-img")
+const githubUrl = "https://github.com/sameer-saharan"
+
+githubImage.addEventListener('click', () => {
+    window.open(githubUrl, "_blank")
+})
+
+
+// Navigation Bar
+
+const jump = function () {
+    const navLinks = document.querySelectorAll('#header-item');
+    console.log(navLinks);
+
+    navLinks.forEach( function(button) {
+        console.log(button);
+        button.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetClass = this.getAttribute('href')
+            const targetSection = document.querySelector(targetClass)
+
+            if (targetSection) {
+                const windowHeight = window.innerHeight;
+                const sectionHeight = targetSection.offsetHeight
+                const offset = (sectionHeight > windowHeight) ? 0 : (windowHeight - sectionHeight) / 2;
+
+                targetSection.scrollIntoView({behaviour: 'smooth', 
+                block: 'center',
+                inline: 'center',
+                offsetTop: offset
+                })
+            }
+        })
+    })
+
+};
+
+document.addEventListener('DOMContentLoaded', jump)
+
+
+
