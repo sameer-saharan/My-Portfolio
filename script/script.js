005dd7d94eea696e17a9ebc292d7c50d00830954
Sameer Saharan <albaadigoru@gmail.com>
2024-04-20 15:14:13 +0530

    Update Project Link

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 // JS Projects
 const image = document.querySelector("#jspr");
 
-const jsprojects = "https://javascript-projects-sameer.vercel.app/"
+const jsprojects = "https://js-sameer.vercel.app/";
 
 image.addEventListener('click', () => {
     window.open(jsprojects, "_blank")
